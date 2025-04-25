99bb5ce4bc762e33a07095a2ac05350a7f00cdb9
Hamza Khan <m.hamzakhan91able@gmail.com>
2025-04-26 00:35:39 +0500

    Added simple back button

--- app/(register)/_layout.tsx
+++ app/(register)/_layout.tsx
@@ -1,12 +1,37 @@
-import { Stack } from "expo-router";
+import { Stack, useNavigation } from "expo-router";
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
+import { IconButton } from "react-native-paper";
+
+function BackButton() {
+  const navigation = useNavigation();
+
+  return (
+    <View>
+      <IconButton
+        icon="arrow-left-circle"
+        size={32}
+        onPress={() => navigation.goBack()}
+        iconColor="white"
+        style={{
+          marginTop: 30,
+          marginLeft: 0,
+        }}
+      />
+    </View>
+  );
+}
 
 export default function _layout() {
   return (
     <Stack
       screenOptions={{
         headerShown: true,
+        title: "",
+        headerLeft: () => <BackButton />,
+        headerStyle: {
+          backgroundColor: "black",
+        },
       }}
     />
   );
