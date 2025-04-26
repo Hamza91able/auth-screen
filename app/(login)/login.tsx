cb77612d3292cc51d7e4154847519455c67ee9e4
Hamza Khan <m.hamzakhan91able@gmail.com>
2025-04-26 15:13:39 +0500

    Added validations and screen enhancements

--- app/(login)/login.tsx
+++ app/(login)/login.tsx
@@ -1,103 +1,193 @@
-import React from "react";
+import React, { useState } from "react";
 import EmailInput from "@/components/TextInputs/EmailInput/EmailInput";
 import PasswordInput from "@/components/TextInputs/PasswordInput/PasswordInput";
-import { View, StyleSheet, Pressable } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Pressable,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from "react-native";
 import { useTheme, Text } from "react-native-paper";
 import Button from "@/components/Button/Button";
 import { useRouter } from "expo-router";
+import { validateEmail } from "@/validations/emailValidators";
+import DangerText from "@/components/Text/DangerText";
 
 export default function login() {
   const theme = useTheme();
   const router = useRouter();
 
+  const [loginData, setLoginData] = useState<{
+    email: string;
+    password: string;
+  }>({
+    email: "",
+    password: "",
+  });
+
+  const [dangerTexts, setDangerTexts] = React.useState<{
+    email: boolean;
+    password: boolean;
+  }>({
+    email: false,
+    password: false,
+  });
+
+  const onChange = (key: keyof typeof loginData, text: string) => {
+    setLoginData({ ...loginData, [key]: text.replace(/\s/g, "") });
+  };
+
+  const validate = () => {
+    const validators = { ...dangerTexts };
+
+    if (!validateEmail(loginData.email)) validators.email = true;
+    else validators.email = false;
+
+    if (loginData.password.length < 8) validators.password = true;
+    else validators.password = false;
+
+    setDangerTexts(validators);
+  };
+
+  const onSubmit = () => {
+    validate();
+  };
+
   return (
-    <>
-      <View
-        style={[
-          styles.container,
-          {
-            flexDirection: "column",
-            backgroundColor: theme.colors.primary,
-          },
-        ]}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+    >
+      <ScrollView
+        contentContainerStyle={styles.scrollContainer}
+        keyboardShouldPersistTaps="handled"
       >
         <View
           style={[
+            styles.container,
             {
-              flexDirection: "row",
+              flexDirection: "column",
+              backgroundColor: theme.colors.primary,
             },
           ]}
         >
-          <Text style={[styles.signUpLabel]}>Login</Text>
-        </View>
-
-        <View
-          style={[
-            {
-              flexDirection: "row",
-            },
-          ]}
-        >
-          <Text style={[styles.signUpDetails]}>
-            Enter your details to login
-          </Text>
-        </View>
-
-        <View
-          style={[
-            {
-              flexDirection: "row",
-            },
-          ]}
-        >
-          <EmailInput />
-        </View>
-
-        <View
-          style={[
-            {
-              flexDirection: "row",
-              marginBottom: 30,
-            },
-          ]}
-        >
-          <PasswordInput label="Password" />
-        </View>
-
-        <View
-          style={[
-            {
-              flexDirection: "row",
-              marginBottom: 30,
-            },
-          ]}
-        >
-          <Button text="Login" />
-        </View>
-
-        <Pressable onPress={() => router.push("/register")}>
-          <Text
-            style={{
-              textAlign: "center",
-              fontSize: 16,
-            }}
+          <View
+            style={[
+              {
+                flexDirection: "row",
+              },
+            ]}
           >
-            Don't have an account?{" "}
+            <Text style={[styles.signUpLabel]}>Login</Text>
+          </View>
+
+          <View
+            style={[
+              {
+                flexDirection: "row",
+              },
+            ]}
+          >
+            <Text style={[styles.signUpDetails]}>
+              Enter your details to login
+            </Text>
+          </View>
+
+          <View
+            style={[
+              {
+                flexDirection: "row",
+              },
+            ]}
+          >
+            <EmailInput
+              onChange={(text) => onChange("email", text)}
+              value={loginData.email}
+            />
+          </View>
+
+          <View
+            style={[
+              {
+                flexDirection: "row",
+                marginTop: 10,
+                marginBottom: 30,
+              },
+            ]}
+          >
+            {dangerTexts.email && (
+              <DangerText text="Email is Not in Valid Format" />
+            )}
+          </View>
+
+          <View
+            style={[
+              {
+                flexDirection: "row",
+              },
+            ]}
+          >
+            <PasswordInput
+              label="Password"
+              onChange={(text) => onChange("password", text)}
+              value={loginData.password}
+            />
+          </View>
+
+          <View
+            style={[
+              {
+                flexDirection: "row",
+                marginTop: 10,
+                marginBottom: 30,
+              },
+            ]}
+          >
+            {dangerTexts.password && (
+              <DangerText text="Password Needs to Be at Least 8 Characters" />
+            )}
+          </View>
+
+          <View
+            style={[
+              {
+                flexDirection: "row",
+                marginBottom: 30,
+              },
+            ]}
+          >
+            <Button text="Login" onPress={onSubmit} />
+          </View>
+
+          <Pressable onPress={() => router.push("/register")}>
             <Text
               style={{
-                fontWeight: "bold",
+                textAlign: "center",
+                fontSize: 16,
               }}
             >
-              Get Started
+              Don't have an account?{" "}
+              <Text
+                style={{
+                  fontWeight: "bold",
+                }}
+              >
+                Get Started
+              </Text>
             </Text>
-          </Text>
-        </Pressable>
-      </View>
-    </>
+          </Pressable>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+  },
   container: {
     flex: 1,
     padding: 20,
